b97d9226095290d1a12d29f2c136c92d8453d16c
Pan Long <aga.panlong@me.com>
2014-05-27 15:21:55 +0800

    reformat visualisation

--- visualisation/Visualisation.java
+++ visualisation/Visualisation.java
@@ -24,53 +24,55 @@ import com.jogamp.newt.event.WindowEvent;
 import com.jogamp.newt.opengl.GLWindow;
 import com.jogamp.opengl.util.FPSAnimator;
 
-import dataReader.dataReader;
 import configuration.ScaleConfiguration;
 
-public class Visualisation implements 
-                   GLEventListener, KeyListener, MouseListener, ActionListener {
+import dataReader.dataReader;
+
+public class Visualisation implements GLEventListener, KeyListener,
+        MouseListener, ActionListener {
 
     static final long serialVersionUID = 1l;
     private static List<point> pointsList = null;
     private static dataReader dr = null;
     private GLJPanel display;
     private Timer animationTimer;
-    private float rotateX, rotateY;   // rotation amounts about axes, controlled by keyboard
-    /* private GLUT glut = new GLUT();  // for drawing the teapot */
+    private float rotateX, rotateY; // rotation amounts about axes, controlled
+                                    // by keyboard
+    /* private GLUT glut = new GLUT(); // for drawing the teapot */
     private GLU glu = new GLU();
     private static ScaleConfiguration sc = null;
-    
+
     private static double scaleFactor;
     private static double radius;
 
-    private static String TITLE = "JOGL 2 with NEWT";  
-    private static final int WINDOW_WIDTH = 640;  
-    private static final int WINDOW_HEIGHT = 480; 
-    private static final int FPS = 60; 
-    
+    private static String TITLE = "JOGL 2 with NEWT";
+    private static final int WINDOW_WIDTH = 640;
+    private static final int WINDOW_HEIGHT = 480;
+    private static final int FPS = 60;
+
     public static void main(String[] args) {
-    	initDataReader();
+        initDataReader();
         GLProfile glp = GLProfile.getDefault();
         GLCapabilities caps = new GLCapabilities(glp);
         GLWindow window = GLWindow.create(caps);
- 
+
         final FPSAnimator animator = new FPSAnimator(window, FPS, true);
-  
+
         window.addWindowListener(new WindowAdapter() {
-           @Override
-           public void windowDestroyNotify(WindowEvent arg0) {
-              // Use a dedicate thread to run the stop() to ensure that the
-              // animator stops before program exits.
-              new Thread() {
-                 @Override
-                 public void run() {
-                    animator.stop(); // stop the animator loop
-                    System.exit(0);
-                 }
-              }.start();
-           };
+            @Override
+            public void windowDestroyNotify(WindowEvent arg0) {
+                // Use a dedicate thread to run the stop() to ensure that the
+                // animator stops before program exits.
+                new Thread() {
+                    @Override
+                    public void run() {
+                        animator.stop(); // stop the animator loop
+                        System.exit(0);
+                    }
+                }.start();
+            };
         });
-  
+
         Visualisation v = new Visualisation();
         window.addGLEventListener(v);
         window.addMouseListener(v);
@@ -81,85 +83,89 @@ public class Visualisation implements
     }
 
     public Visualisation() {
-//        GLCapabilities caps = new GLCapabilities(null);
-//        display = new GLJPanel(caps);
-//        display.setPreferredSize( new Dimension(600,600) );  // TODO: set display size here
-//        display.addGLEventListener(this);
-//        setLayout(new BorderLayout());
-//        add(display,BorderLayout.CENTER);
-        // TODO:  Other components could be added to the main panel.
-        
-//        rotateX = 15;  // initialize some variables used in the drawing.
-//        rotateY = 15;
-        
-        // TODO:  Uncomment the next two lines to enable keyboard event handling
-//        requestFocusInWindow();
-//        display.addKeyListener(this);
+        // GLCapabilities caps = new GLCapabilities(null);
+        // display = new GLJPanel(caps);
+        // display.setPreferredSize( new Dimension(600,600) ); // TODO: set
+        // display size here
+        // display.addGLEventListener(this);
+        // setLayout(new BorderLayout());
+        // add(display,BorderLayout.CENTER);
+        // TODO: Other components could be added to the main panel.
 
-        // TODO:  Uncomment the next one or two lines to enable mouse event handling
-//        display.addMouseListener(this);
-//        display.addMouseMotionListener(this);
-        
+        // rotateX = 15; // initialize some variables used in the drawing.
+        // rotateY = 15;
+
+        // TODO: Uncomment the next two lines to enable keyboard event handling
+        // requestFocusInWindow();
+        // display.addKeyListener(this);
+
+        // TODO: Uncomment the next one or two lines to enable mouse event
+        // handling
+        // display.addMouseListener(this);
+        // display.addMouseMotionListener(this);
+
         // TODO: Uncomment the following line to start the animation
-        /* startAnimation();  */
+        /* startAnimation(); */
     }
-    
-    public static void initDataReader(){
-    	dr = new dataReader("output1.pcd");
-    	pointsList = dr.getPoints();
+
+    public static void initDataReader() {
+        dr = new dataReader("output1.pcd");
+        pointsList = dr.getPoints();
         sc = new ScaleConfiguration(pointsList, 10);
         scaleFactor = sc.getScaleFactor();
         radius = sc.getRadius();
     }
 
-    // ---------------  Methods of the GLEventListener interface -----------
-    public void buildPoints(GLAutoDrawable drawable){
-    	GL2 gl = drawable.getGL().getGL2();
+    // --------------- Methods of the GLEventListener interface -----------
+    public void buildPoints(GLAutoDrawable drawable) {
+        GL2 gl = drawable.getGL().getGL2();
         gl.glEnable(GL2.GL_POINT_SPRITE); // GL_POINT_SPRITE_ARB if you're
         gl.glEnable(GL2.GL_POINT_SMOOTH);
         gl.glEnable(GL2.GL_BLEND);
         gl.glBlendFunc(GL2.GL_SRC_ALPHA, GL2.GL_ONE_MINUS_SRC_ALPHA);
-        gl.glPointSize((float)radius);
+        gl.glPointSize((float) radius);
 
-    	gl.glBegin(GL.GL_POINTS);
-    	for (point p : pointsList) {
-			gl.glPushMatrix();
-			gl.glTranslatef(p.getX(), p.getY(), p.getZ());
-			/* gl.glColor3f(0.95f, 0.207f, 0.031f); */
-			gl.glColor3f(0.95f, 0.207f, 0.031f);
-			gl.glVertex3f((float)(p.getX() * scaleFactor), (float)(p.getY() * scaleFactor), (float)(p.getZ() * scaleFactor));
-			gl.glPopMatrix();
-		}
-    	gl.glEnd();
+        gl.glBegin(GL.GL_POINTS);
+        for (point p : pointsList) {
+            gl.glPushMatrix();
+            gl.glTranslatef(p.getX(), p.getY(), p.getZ());
+            /* gl.glColor3f(0.95f, 0.207f, 0.031f); */
+            gl.glColor3f(0.95f, 0.207f, 0.031f);
+            gl.glVertex3f((float) (p.getX() * scaleFactor),
+                    (float) (p.getY() * scaleFactor),
+                    (float) (p.getZ() * scaleFactor));
+            gl.glPopMatrix();
+        }
+        gl.glEnd();
     }
 
     /**
      * This method is called when the OpenGL display needs to be redrawn.
      */
-    public void display(GLAutoDrawable drawable) {  
-            // called when the panel needs to be drawn
+    public void display(GLAutoDrawable drawable) {
+        // called when the panel needs to be drawn
 
         GL2 gl = drawable.getGL().getGL2();
-        gl.glClearColor(0.8f,0.8f,0.8f,0);
-        gl.glClear( GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT );
+        gl.glClearColor(0.8f, 0.8f, 0.8f, 0);
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
 
-        gl.glMatrixMode(GL2.GL_PROJECTION);  // TODO: Set up a better projection?
+        gl.glMatrixMode(GL2.GL_PROJECTION); // TODO: Set up a better projection?
         gl.glLoadIdentity();
         /* gl.glOrtho(-1,1,-1,1,-2,2); */
         glu.gluPerspective(35, 1, 0.1, 10000);
         glu.gluLookAt(0, 0, 40, 0, 0, 0, 0, 1, 0);
-        
+
         gl.glMatrixMode(GL2.GL_MODELVIEW);
 
-        gl.glLoadIdentity();             // Set up modelview transform. 
-        gl.glRotatef(rotateY,0,1,0);
-        gl.glRotatef(rotateX,1,0,0);
+        gl.glLoadIdentity(); // Set up modelview transform.
+        gl.glRotatef(rotateY, 0, 1, 0);
+        gl.glRotatef(rotateX, 1, 0, 0);
 
-        // TODO: add drawing code!!  As an example, draw a GLUT teapot
+        // TODO: add drawing code!! As an example, draw a GLUT teapot
         buildPoints(drawable);
         /* gl.glColor3f(1.0f, 1.0f, 1.0f); */
         /* glut.glutSolidTeapot(5); */
-//        glut.glutSolidSphere(1.0, 10, 10);
+        // glut.glutSolidSphere(1.0, 10, 10);
 
         /* gl.glEnable( GL2.GL_POINT_SPRITE ); // GL_POINT_SPRITE_ARB if you're */
         /* gl.glEnable( GL2.GL_POINT_SMOOTH ); */
@@ -167,7 +173,7 @@ public class Visualisation implements
         /* gl.glBlendFunc( GL2.GL_SRC_ALPHA, GL2.GL_ONE_MINUS_SRC_ALPHA ); */
         /* gl.glPointSize(10f); */
         /* gl.glBegin(GL.GL_POINTS); */
-        /* gl.glColor3f( 0.95f, 0.207f, 0.031f  ); */
+        /* gl.glColor3f( 0.95f, 0.207f, 0.031f ); */
         /* gl.glVertex3f( 0.5f, 0.5f, 0.5f); */
         /* gl.glVertex3f( 0.5f, 0.6f, 0.6f); */
         /* gl.glVertex3f( 0.5f, 0.8f, 0.7f); */
@@ -179,8 +185,8 @@ public class Visualisation implements
     }
 
     /**
-     * This is called when the GLJPanel is first created.  It can be used to initialize
-     * the OpenGL drawing context.
+     * This is called when the GLJPanel is first created. It can be used to
+     * initialize the OpenGL drawing context.
      */
     public void init(GLAutoDrawable drawable) {
         // called when the panel is created
@@ -191,12 +197,11 @@ public class Visualisation implements
         gl.glEnable(GL2.GL_LIGHT0);
         gl.glEnable(GL2.GL_COLOR_MATERIAL);
         doLighting(gl);
-        
+
         buildPoints(drawable);
     }
 
-    private void doLighting( GL2 gl )
-    {
+    private void doLighting(GL2 gl) {
         float[] lightPos = new float[4];
         lightPos[0] = 50005;
         lightPos[1] = 30000;
@@ -205,9 +210,9 @@ public class Visualisation implements
         gl.glEnable(GL2.GL_DEPTH_TEST);
         gl.glEnable(GL2.GL_LIGHTING);
         gl.glEnable(GL2.GL_LIGHT0);
-        float[] noAmbient ={ 0.1f, 0.1f, 0.1f, 1f }; // low ambient light
-        float[] spec =    { 1f, 0f, 0.6f, 1f }; // low ambient light
-        float[] diffuse ={ 1f, 1f, 1f, 1f };
+        float[] noAmbient = { 0.1f, 0.1f, 0.1f, 1f }; // low ambient light
+        float[] spec = { 1f, 0f, 0.6f, 1f }; // low ambient light
+        float[] diffuse = { 1f, 1f, 1f, 1f };
         // properties of the light
         gl.glLightfv(GL2.GL_LIGHT0, GL2.GL_AMBIENT, noAmbient, 0);
         gl.glLightfv(GL2.GL_LIGHT0, GL2.GL_SPECULAR, spec, 0);
@@ -226,11 +231,11 @@ public class Visualisation implements
         /* gl.glLightfv(GL2.GL_LIGHT1, GL2.GL_SPECULAR, spec, 0); */
         /* gl.glLightfv(GL2.GL_LIGHT1, GL2.GL_DIFFUSE, diffuse, 0); */
         /* gl.glLightfv(GL2.GL_LIGHT1, GL2.GL_POSITION, lightPos1, 0); */
-        /* float[] colorBlack  = {0.0f,0.0f,0.0f,1.0f}; */
-        /* float[] colorWhite  = {1.0f,1.0f,1.0f,1.0f}; */
-        /* float[] colorGray   = {0.6f,0.6f,0.6f,1.0f}; */
-        /* float[] colorRed    = {1.0f,0.0f,0.0f,1.0f}; */
-        /* float[] colorBlue   = {0.0f,0.0f,0.1f,1.0f}; */
+        /* float[] colorBlack = {0.0f,0.0f,0.0f,1.0f}; */
+        /* float[] colorWhite = {1.0f,1.0f,1.0f,1.0f}; */
+        /* float[] colorGray = {0.6f,0.6f,0.6f,1.0f}; */
+        /* float[] colorRed = {1.0f,0.0f,0.0f,1.0f}; */
+        /* float[] colorBlue = {0.0f,0.0f,0.1f,1.0f}; */
         /* float[] colorYellow = {1.0f,1.0f,0.0f,1.0f}; */
         /* float[] colorLightYellow = {.5f,.5f,0.0f,1.0f}; */
         /* // First Switch the lights on. */
@@ -241,12 +246,12 @@ public class Visualisation implements
         /*  */
         /* // */
         /* // Light 0. */
-        /* //     */
+        /* // */
         /* // Default from the red book. */
         /* // */
         /* float[] noAmbient ={ 0.3f, 0.3f, 0.3f, 1f }; // low ambient light */
         /* gl.glLightfv( GL2.GL_LIGHT0, GL2.GL_AMBIENT, noAmbient, 0); */
-        /* gl.glLightfv( GL2.GL_LIGHT0, GL2.GL_POSITION, lightPos, 0);     */
+        /* gl.glLightfv( GL2.GL_LIGHT0, GL2.GL_POSITION, lightPos, 0); */
         /*  */
         /* // */
         /* // Light 1. */
@@ -280,48 +285,50 @@ public class Visualisation implements
     }
 
     /**
-     * 
-     * Called when the size of the GLJPanel changes.  Note:  glViewport(x,y,width,height)
-     * has already been called before this method is called!
+     *
+     * Called when the size of the GLJPanel changes. Note:
+     * glViewport(x,y,width,height) has already been called before this method
+     * is called!
      */
-    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width,
+            int height) {
     }
 
     /**
-     * This is called before the GLJPanel is destroyed.  It can be used to release OpenGL resources.
+     * This is called before the GLJPanel is destroyed. It can be used to
+     * release OpenGL resources.
      */
     public void dispose(GLAutoDrawable drawable) {
     }
 
-
-    // ------------ Support for keyboard handling  ------------
+    // ------------ Support for keyboard handling ------------
 
     /**
      * Called when the user presses any key.
      */
     public void keyPressed(KeyEvent e) {
-        int key = e.getKeyCode();  // Tells which key was pressed.
-        if ( key == KeyEvent.VK_LEFT )
+        int key = e.getKeyCode(); // Tells which key was pressed.
+        if (key == KeyEvent.VK_LEFT)
             rotateY -= 15;
-        else if ( key == KeyEvent.VK_RIGHT )
+        else if (key == KeyEvent.VK_RIGHT)
             rotateY += 15;
-        else if ( key == KeyEvent.VK_DOWN)
+        else if (key == KeyEvent.VK_DOWN)
             rotateX += 15;
-        else if ( key == KeyEvent.VK_UP )
+        else if (key == KeyEvent.VK_UP)
             rotateX -= 15;
-        else if ( key == KeyEvent.VK_HOME )
+        else if (key == KeyEvent.VK_HOME)
             rotateX = rotateY = 0;
-        //repaint();
+        // repaint();
     }
 
     /**
      * Called when the user types a character.
      */
-    public void keyTyped(KeyEvent e) { 
-        /* char ch = e.getKeyChar();  // Which character was typed. */
+    public void keyTyped(KeyEvent e) {
+        /* char ch = e.getKeyChar(); // Which character was typed. */
     }
 
-    public void keyReleased(KeyEvent e) { 
+    public void keyReleased(KeyEvent e) {
     }
 
     // --------------------------- animation support ---------------------------
@@ -331,13 +338,13 @@ public class Visualisation implements
     private boolean animating;
 
     private void updateFrame() {
-        // TODO:  add any other updating required for the next frame.
+        // TODO: add any other updating required for the next frame.
         rotateY += 10;
         /* frameNumber++; */
     }
 
     public void startAnimation() {
-        if ( ! animating ) {
+        if (!animating) {
             if (animationTimer == null) {
                 animationTimer = new Timer(30, this);
             }
@@ -358,9 +365,7 @@ public class Visualisation implements
         display.repaint();
     }
 
-
-
-    // MouseEvent support 
+    // MouseEvent support
     private boolean dragging = false;
     private double prevX, prevY;
 
@@ -371,7 +376,7 @@ public class Visualisation implements
     @Override
     public void mouseDragged(com.jogamp.newt.event.MouseEvent arg0) {
         // TODO Auto-generated method stub
-        if (! dragging) {
+        if (!dragging) {
             return;
         }
 
@@ -380,7 +385,7 @@ public class Visualisation implements
 
         double mouseDeltaX = x - prevX;
         double mouseDeltaY = y - prevY;
-        // TODO:  respond to mouse drag to new point (x,y)   
+        // TODO: respond to mouse drag to new point (x,y)
 
         if ((rotateY % 360 > 90 && rotateY % 360 < 270)
                 || (rotateY % 360 < 0 && rotateY % 360 + 360 > 90 && rotateY % 360 + 360 < 270))
@@ -420,25 +425,25 @@ public class Visualisation implements
     public void mousePressed(com.jogamp.newt.event.MouseEvent arg0) {
         // TODO Auto-generated method stub
         if (dragging) {
-            return;  // don't start a new drag while one is already in progress
+            return; // don't start a new drag while one is already in progress
         }
         int x = arg0.getX();
         int y = arg0.getY();
 
         // TODO: respond to mouse click at (x,y)
-        dragging = true;  // might not always be correct!
+        dragging = true; // might not always be correct!
         /* prevX = startX = x; */
         /* prevY = startY = y; */
         prevX = x;
         prevY = y;
-        /* display.repaint();   */
+        /* display.repaint(); */
 
     }
 
     @Override
     public void mouseReleased(com.jogamp.newt.event.MouseEvent arg0) {
         // TODO Auto-generated method stub
-        if (! dragging) {
+        if (!dragging) {
             return;
         }
         dragging = false;
@@ -450,4 +455,4 @@ public class Visualisation implements
         // TODO Auto-generated method stub
 
     }
-                   }
+}
